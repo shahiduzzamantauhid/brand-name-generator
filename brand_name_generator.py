e2dceede269abcb652a97cf2fe9a4149217d26d1
tauhid143 <shahiduzzamantauhid@gmail.com>
2021-12-03 21:25:05 +0000

    first commit

---
+++ brand_name_generator.py
@@ -0,0 +1,9 @@
+#start the program of brand name generator 
+print("Do you want to create a your brand name?")
+print("Enter some information over here...\n")
+# collect city name from here
+EnterCity= input("Enter your city here :\n")
+#collect pet name from here
+EnterPetName = input("Enter your pet name here :\n")
+#print full name here
+print("Your Full name is : " + EnterCity + " " + EnterPetName)
